593b399fb223d8855a3c8530c23ddfa7ec527b08
Uwe <uwehrmnn@gmail.com>
2011-05-22 04:04:09 +0200

    floating VBox

--- dwm-5.8.2/config.def.h
+++ dwm-5.8.2/config.def.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
   { "Gimp",           NULL,       NULL,          1 << 2,       True,        -1 },
   { "Gajim.py",       NULL,       NULL,          1 << 3,       False,       -1 },
   { NULL,             NULL,       "LibreOffice", 1 << 3,       False,       -1 },
-  { "VirtualBox",     NULL,       NULL,          1 << 3,       False,       -1 },
+  { "VirtualBox",     NULL,       NULL,          1 << 3,       True,        -1 },
   { "feh",            NULL,       NULL,          0,            True,        -1 },
   { "MPlayer",        NULL,       NULL,          0,            True,        -1 },
   { NULL,             NULL,       "Save file",   0,            True,        -1 },
