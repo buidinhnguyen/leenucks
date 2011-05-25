1f267cf62052ab4d12b97230f0a29813a8dc2c66
Uwe <uwehrmnn@gmail.com>
2011-05-26 01:52:10 +0200

    initial commit

---
+++ scripts/google_reader_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Requirements:
+# working oauth2-token for google account
+# curl
+# 
+
+DT=$(date +"%Y%m%d")
+
+OAUTH_TOKEN=$(awk '/^access_token/ { print $2 }' ${XDG_CONFIG_HOME}/google_oauth2/access_token)
+BACKUP_FILE="${HOME}/files/backups/Subscriptions-${DT}.opml"
+
+curl -s -o "${BACKUP_FILE}" -H "Authorization: OAuth ${OAUTH_TOKEN}" https://www.google.com/reader/subscriptions/export
+
