3ab33e6025bd84a2cf9e830bb35fe95db629b514
Uwe <uwehrmnn@gmail.com>
2011-05-26 17:40:01 +0200

    gmail unread count

--- google_mail_unread.sh
+++ google_mail_unread.sh
@@ -9,4 +9,8 @@
 # Requirements:
 # curl
 
+source Google_OAuth2.sh
+
+OAUTH_TOKEN="$(awk '/^access/ { print $2 }' ${DATADIR}/access_token )"
+
 curl -s -H "Authorization: OAuth ${OAUTH_TOKEN}" "https://mail.google.com/mail/feed/atom" | sed -e 's/<\// /g;s/>/ /g' | awk '/^<fullcount/ { print $2 }'
