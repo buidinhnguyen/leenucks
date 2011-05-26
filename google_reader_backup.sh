fb03f222014f5918e00811d40b10aa7a8c36e02d
Uwe <uwehrmnn@gmail.com>
2011-05-26 17:41:27 +0200

    reader_backup.sh update

--- google_reader_backup.sh
+++ google_reader_backup.sh
@@ -9,14 +9,13 @@
 # Requirements:
 # curl
 #
-# TODO:
-# Rewrite 
-# 
+
+source Google_OAuth2.sh
+
+OAUTH_TOKEN="$(awk '/^access/ { print $2 }' ${DATADIR}/access_token )"
+BACKUP_FILE="${HOME}/files/backups/Subscriptions-${DT}.opml"
 
 DT=$(date +"%Y%m%d")
-
-OAUTH_TOKEN=$(awk '/^access_token/ { print $2 }' ${XDG_CONFIG_HOME}/google_oauth2/access_token)
-BACKUP_FILE="${HOME}/files/backups/Subscriptions-${DT}.opml"
 
 curl -s -o "${BACKUP_FILE}" -H "Authorization: OAuth ${OAUTH_TOKEN}" https://www.google.com/reader/subscriptions/export
 
