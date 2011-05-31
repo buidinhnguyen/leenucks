5b91c71e45ae275104f33d25cc9ea2d66784593d
Uwe <uleenucks@gmail.com>
2011-05-31 18:54:47 +0200

    lbdb as a an address book for mutt and refresh_script

---
+++ bin/refresh_address.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# License: GPLv2 or later
+# by tomka (http://dev.gentoo.org/~tomka/mail.html)
+
+acc1=~/Mail/GMail/all_mail/cur
+acc2=~/Mail/personal/all_mail/cur
+
+parsemail () {
+    cat $1 | lbdb-fetchaddr
+}
+
+parsemaildir () {
+    for mailfile in $( find $1 -type f -mtime -5 ) ; do
+        parsemail ${mailfile}
+    done
+}
+
+# The IFS variable saves the file name separator 
+# which we will temporarily set to \n so that the
+# spaces in Gmail folders will work
+
+for i in "${acc1}" "${acc2}" ; do 
+    o=${IFS}
+    IFS=$(echo -en "\n\b")
+    parsemaildir "${i}"
+    IFS=o
+done
