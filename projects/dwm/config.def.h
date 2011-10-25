e1d6e55f5d717b932d5cc5b4ede19e7cf7ebd226
Uwe <uleenucks@gmail.com>
2011-10-25 10:33:02 +0200

    checkupdates

--- projects/dwm/config.def.h
+++ projects/dwm/config.def.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
   { NULL,             NULL,       "LibreOffice", 1 << 2,       False,       -1 },
   { "feh",            NULL,       NULL,          0,            True,        -1 },
   { "openttd",        NULL,       NULL,          0,            True,        -1 },
+  { "sxiv",           NULL,       NULL,          0,            True,        -1 },
   { "MPlayer",        NULL,       NULL,          0,            True,        -1 },
   { "XFontSel",       NULL,       NULL,          0,            True,        -1 },
   { "Zathura",        NULL,       NULL,          0,            True,        -1 },
