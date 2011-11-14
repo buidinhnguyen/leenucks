89c62240d3373f041140849561b9adacbf9dc58d
Uwe <uleenucks@gmail.com>
2011-11-14 10:37:02 +0100

    small rewrite on leave.sh

--- bin/leave.sh
+++ bin/leave.sh
@@ -4,13 +4,6 @@
 # a simple logout dialog
 #
 ###
-# kill_apps {{{1
-kill_apps() {
-	while read -r app; do
-		wmctrl -i -c "$app"
-	done < <(wmctrl -l | awk '{print $1}')
-}
-# choice_function {{{1
 choice_by_dmenu() {
 if [[ -f "${HOME}/.dmenurc" ]]; then
 	. "${HOME}/.dmenurc"
@@ -20,15 +13,15 @@ fi
 
 	choice="$(echo -e "a: Logout\nr: Reboot\ns: Shutdown" | $DMENU | cut -d ':' -f 1)"
 }
-# are we on X and is a choice being used? {{{1
+
+# are we on X and is a choice being used?
 [[ -z "$DISPLAY" ]] && exit 1
 
 choice_by_dmenu
 
 [[ -z "$choice" ]] && exit 1
-# gracefully close all open apps {{{1
-kill_apps
-# execute the choice in background {{{1
+
+# execute the choice in background 
 case "$choice" in
 	a) pkill X &     ;;
 	r) sudo init 6 & ;;
